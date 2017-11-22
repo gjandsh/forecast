40e103cf6612de2e3230b88ac9e644468fdca08d
gjandsh <gjandsh@126.com>
2017-11-22 13:41:49 +0800

    增加后端接口

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var url = {
+var urls = {
     coverimgs: [["https://i.loli.net/2017/11/21/5a13da3d3209d.jpg", "https://i.loli.net/2017/11/21/5a13da3bda99d.jpg"],
     ["https://i.loli.net/2017/11/21/5a13da3be5ded.jpg", "https://i.loli.net/2017/11/21/5a13da3b1f149.jpg"],
     ["https://i.loli.net/2017/11/21/5a13da3c26c40.jpg", "https://i.loli.net/2017/11/21/5a13da3b043c0.jpg"],
@@ -12,7 +12,7 @@ var url = {
     ["https://i.loli.net/2017/11/21/5a13de3b40b69.jpg", "https://i.loli.net/2017/11/21/5a13de24f30d9.jpg"],
     ["https://i.loli.net/2017/11/21/5a13de6875083.jpg", "https://i.loli.net/2017/11/21/5a13de572f7f9.jpg"],
     ["https://i.loli.net/2017/11/21/5a13de8863bc9.jpg", "https://i.loli.net/2017/11/21/5a13de759df2c.jpg"],
-    ["https://i.loli.net/2017/11/21/5a13dea16e15d.jpg", "https://i.loli.net/2017/11/21/5a13de981c6ce.jpg"],
+    ["https://i.loli.net/2017/11/21/5a13de981c6ce.jpg", "https://i.loli.net/2017/11/21/5a13dea16e15d.jpg"],
     ["https://i.loli.net/2017/11/21/5a13deb2983cb.jpg", "https://i.loli.net/2017/11/21/5a13deaa9bfac.jpg"],
     ["https://i.loli.net/2017/11/21/5a13dec1288f6.jpg", "https://i.loli.net/2017/11/21/5a13deba58300.jpg"],
     ["https://i.loli.net/2017/11/21/5a13decd26dd2.jpg", "https://i.loli.net/2017/11/21/5a13dec772785.jpg"],
@@ -40,9 +40,104 @@ var url = {
     ["https://i.loli.net/2017/11/21/5a13e3fdca5bc.jpg", "https://i.loli.net/2017/11/21/5a13e4031f3d5.jpg"],
     ["https://i.loli.net/2017/11/21/5a13e46a81702.jpg", "https://i.loli.net/2017/11/21/5a13e47134a41.jpg"]
     ],
-    weathericons: ["icon-qing", "icon-qing", "icon-qing", "icon-qing", "icon-duoyun", "icon-duoyun1", "icon-duoyun1", "icon-duoyun1", "icon-duoyun1",
-        "icon-duoyun1", "icon-zhenyu", "icon-leiyu", "icon-leizhenyubanyoubingbao", "icon-xiaoyu", "icon-yu", "icon-yu", "icon-tianqizitiku30",
+    weathericons: ["icon-qing", "icon-qing", "icon-qing", "icon-qing", "icon-duoyun1", "icon-duoyun1", "icon-duoyun1", "icon-duoyun1", "icon-duoyun1",
+        "icon-duoyun", "icon-zhenyu", "icon-leiyu", "icon-leizhenyubanyoubingbao", "icon-xiaoyu", "icon-yu", "icon-yu", "icon-tianqizitiku30",
         "icon-tedabaoyu", "icon-dongyu", "icon-161", "icon-zhenxue", "icon-xiaoxue", "icon-zhongxuemoderatesnow", "icon-daxue", "icon-baoxue", "icon-27",
         "icon-blowingSand", "icon-shachenbao", "icon-shachenbao", "icon-wu", "icon-mai1", "icon-feng", "icon-dafeng", "icon-tianqizitiku17", "icon-19redaifengbao",
         "icon-longjuanfeng", "icon-leng", "icon-re", "icon-tianqizitiku51"]
+};
+var urlcode = [];
+var todayimg = [];
+function pad(num) {
+    if (2 == num.toString().length) return num;
+    return "0" + num;
 }
+setInterval(function () {
+    let date = new Date();
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
+    let timing = hours < 12 ? pad(hours) + ':' + pad(minutes) + 'am' : pad(hours) + ':' + pad(minutes) + ' pm'
+    $('.timing').text(timing);
+}, 300)
+$.get('https://weixin.jirengu.com/weather/ip')
+    .done(function (res) {
+        let dataip = res.data;
+        $.get('https://weixin.jirengu.com/weather/cityid',
+            { location: dataip }).done(function (res) {
+                let datacity = res.results[0].name
+                let datacountry = res.results[0].path.slice(9, 11);
+                $('.location .city').children().first().text(datacity + ',');
+                $('.location .country').text(datacountry);
+                let cid = res.results[0].id;
+                $.get('https://weixin.jirengu.com/weather/now',
+                    { cityid: cid }).done(function (res) {
+                        let datacode = res.weather[0].now.code;
+                        $('.wrap').css('background-image', `url(${urls.coverimgs[datacode][1]}`);
+                        $('.cover').css('background-image', `url(${urls.coverimgs[datacode][0]}`);
+                        todayimg.push(urls.coverimgs[datacode][1])
+                        todayimg.push(urls.coverimgs[datacode][0])
+                        let $weaic = $('.weather-content').find('li').first().find('.weather-icon');
+                        for (i = 0; i < 2; i++) {
+                            let $nowicon = $($weaic[i]).children('i');
+                            let preicon = $nowicon[0].classList.item(1);
+                            $nowicon.removeClass(preicon).addClass(urls.weathericons[datacode]);
+                        }
+                        console.log(res.weather[0])
+                        for (i = 0; i < 7; i++) {
+                            let avetem = Math.floor((parseInt(res.weather[0].future[i].high) + parseInt(res.weather[0].future[i].low)) / 2);
+                            $('.today').eq(i).children('.temperature').text(avetem + '°');
+                            let futemrange = res.weather[0].future[i].low + "°~" + res.weather[0].future[i].high;
+                            $('.not-today').eq(i).children('.temperature').text(futemrange + "°");
+                            let nowtem = res.weather[0].now.temperature
+                            $('.today').eq(0).children('.temperature').text(nowtem + '°');
+                            let futext = res.weather[0].future[i].text;
+                            $('.today').eq(i).children('.description').text(futext);
+                            var datatext = res.weather[0].now.text;
+                            $('.today').eq(0).children('.description').text(datatext);
+                            let fucode = res.weather[0].future[i].code2;
+                            urlcode.push(res.weather[0].future[i].code2);
+                            let date = new Date(res.weather[0].future[i].date);
+                            let weekday = res.weather[0].future[i].day
+                            $('.today .date').eq(i).text((pad(date.getMonth() + 1)) + '-' + pad(date.getDate()) + " " + weekday);
+                            $('.not-today .day').eq(i).children().text(weekday);
+                            console.log(fucode);
+                            let $fuicon = $('.weather-content').find('li').eq(i).find('.weather-icon');
+                            for (j = 0; j < 2; j++) {
+                                let $funowicon = $($fuicon[j]).children('i');
+                                let fupreicon = $funowicon[0].classList.item(1);
+                                $funowicon.removeClass(fupreicon).addClass(urls.weathericons[fucode]);
+                            }
+                            let $weaic = $('.weather-content').find('li').first().find('.weather-icon');
+                            for (k = 0; k < 2; k++) {
+                                let $nowicon = $($weaic[k]).children('i');
+                                let preicon = $nowicon[0].classList.item(1);
+                                $nowicon.removeClass(preicon).addClass(urls.weathericons[datacode]);
+                            }
+                        }
+                    })
+            })
+    }).done(
+    function () {
+        let $weaCon = $('.weather-content')
+        $weaCon.on('click', '.not-today', function () {
+            $(this).addClass('inactive');
+            $(this).parent().children('.today').removeClass('inactive');
+            $(this).parent().siblings().children('.today').addClass('inactive');
+            $(this).parent().siblings().children('.not-today').removeClass('inactive');
+            let codeindex = $(this).parent().index()
+            if (codeindex === 0) {
+                $('.wrap').css('background-image', `url(${todayimg[0]}`);
+                $('.cover').css('background-image', `url(${todayimg[1]}`);
+            } else {
+                $('.wrap').css('background-image', `url(${urls.coverimgs[urlcode[codeindex]][1]}`);
+                $('.cover').css('background-image', `url(${urls.coverimgs[urlcode[codeindex]][0]}`);
+            }
+        });
+        $weaCon.on('mouseenter', '.not-today', function () {
+            $(this).addClass('hover');
+        })
+        $weaCon.on('mouseleave', '.not-today', function () {
+            $(this).removeClass('hover');
+        })
+    }
+    )
